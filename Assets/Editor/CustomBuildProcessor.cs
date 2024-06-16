a043f3126e97903b90c711856d868d3a673505b9
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2024-06-16 15:10:17 +0300

    uaction

--- Assets/Editor/CustomBuildProcessor.cs
+++ Assets/Editor/CustomBuildProcessor.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 
 public static class CustomBuildProcessor
 {
@@ -13,8 +14,12 @@ public static class CustomBuildProcessor
             options = BuildOptions.None
         };
 
+        Debug.Log("Starting pre-build processing...");
         Naninovel.BuildProcessor.PreprocessBuild(options);
+        Debug.Log("Building player...");
         BuildPipeline.BuildPlayer(options);
+        Debug.Log("Starting post-build processing...");
         Naninovel.BuildProcessor.PostprocessBuild();
+        Debug.Log($"Build completed. File saved to: {options.locationPathName}");
     }
 }
